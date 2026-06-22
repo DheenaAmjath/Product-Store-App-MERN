09b82480a80216cc6f821616421b60eb4e5c02c1
DheenaAmjath <dheenaamjath8@gmail.com>
2026-06-22 23:37:43 +0530

    Added product API backend with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config /db.js';
+import productRoutes from "./routes/product.routes.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-});
+app.use(express.json());//allows us to accept JSON data in the req.body
 
-app.listen(8000, ()=>{
-    console.log("Server started at http://localhost:8000");
-    });
+app.use("/api/products",productRoutes);
+
+connectDB()
+  .then(() => {
+      app.listen(8000, () => {
+          console.log("Server started");
+      });
+  });
